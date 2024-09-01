617280f63c2df6fa5249d2d1866838457bb75d54
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2024-09-01 09:48:41 +0800

    Update eslintrc

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,10 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true,
+    es2020: true,
+    node: true,
+  },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
@@ -9,12 +13,11 @@ module.exports = {
     'plugin:@stylistic/recommended-extends',
     './.eslintrc-auto-import.json',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: ['dist'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
   plugins: [
     'react-refresh',
-    '@stylistic',
   ],
   rules: {
     'react-refresh/only-export-components': [
@@ -22,7 +25,7 @@ module.exports = {
       { allowConstantExport: true },
     ],
     'react/prop-types': 'off',
-    '@stylistic/brace-style': 'off',
+    '@stylistic/brace-style': ['error', '1tbs'],
     '@stylistic/multiline-ternary': 'off',
   },
 }
